dd2ae4bb48887c502139f6cd79ef99e64244de1f
Abdurrahman-Alizada <abdurrahmanalizada17@gmail.com>
2022-05-18 15:05:09 +0500

    setup is done. ready for contract development

---
+++ contracts/NFT.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
+
+contract NFT is ERC721URIStorage{
+    // auto-increment field for each NFT
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    
+    address contractAddress;
+
+    constructor(address marketPlaceAddress) ERC721("GULAB", "GULAB"){
+        contractAddress = marketPlaceAddress;
+    }
+
+    function createToke( string memory tokenURI ) public returns(uint256) {
+      _tokenIds.increment();
+      uint256 newItemId = _tokenIds.current();
+      _mint(msg.sender, newItemId);
+      _setTokenURI(newItemId, tokenURI);
+      setApprovalForAll(contractAddress, true);
+      return newItemId;
+    }
+}
---
+++ contracts/NFTMarket.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/utils/Counters.sol';
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
+
+contract NFTMarket is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemIds;
+    Counters.Counter private _itemsSold;
+
+    address payable owner;
+    
+}
